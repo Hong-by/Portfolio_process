62c4095ac23b0a0570ec2e960438979dd874228b
HongBY <hby033@naver.com>
2021-06-04 18:28:03 +0900

    06/04 메인 페이지 디자인 대략 적으로 완료/ mySQL DB 구성 완료 / 각 파트 별 페이지 제작 예정

--- js/index.js
+++ js/index.js
@@ -22,15 +22,23 @@ document.addEventListener('DOMContentLoaded', function() {
 
   let lWidth = 10;
   let tWidth = 8;
+
+  let eachSize = 90;
+  let pieSize = 200;
+  let clearSet;
   const winWidth = window.innerWidth;
 
-  if(winWidth <= 950) {
-    lWidth = 5;
-    tWidth = 4;
+  
+  if(winWidth <= 1280 && winWidth > 950) {
+    pieSize = 180;
+  } else if(winWidth <= 950 && winWidth > 400){
+    pieSize = 170;
+  } else if( winWidth <= 400){
+    pieSize = 140;
   } else {
-    lWidth = 10;
-    tWidth = 8;
+    pieSize = 200;
   }
+
 
   var chart = window.chart = new EasyPieChart(document.querySelector('.total-chart .chart'), {
     easing: 'easeOutElastic',
@@ -40,12 +48,66 @@ document.addEventListener('DOMContentLoaded', function() {
     scaleColor: false,
     lineWidth: 18,
     trackWidth: 18,
-    size:200,
+    size: pieSize,
     lineCap: 'butt',
     onStep: function(from, to, percent) {
-       this.el.children[0].innerHTML = Math.round(percent);
+      this.el.children[0].innerHTML = Math.round(percent);
     }
- });
+  });
+
+
+  window.addEventListener('resize', function(){
+    const winWidth = window.innerWidth;
+
+    if(winWidth <= 1280 && winWidth > 950) {
+      pieSize = 180;
+    } else if(winWidth <= 950 && winWidth > 400){
+      pieSize = 170;
+    } else if( winWidth <= 400){
+      pieSize = 140;
+    } else {
+      pieSize = 200;
+    }
+
+
+    clearTimeout(clearSet);
+
+    clearSet = setTimeout(function(){
+
+      document.querySelector('.total-chart .chart canvas').remove();
+      var chart = window.chart = new EasyPieChart(document.querySelector('.total-chart .chart'), {
+        easing: 'easeOutElastic',
+        delay: 3000,
+        barColor: '#7c41f5',
+        trackColor: '#c1a3ff',
+        scaleColor: false,
+        lineWidth: 18,
+        trackWidth: 18,
+        size: pieSize,
+        lineCap: 'butt',
+        onStep: function(from, to, percent) {
+          this.el.children[0].innerHTML = Math.round(percent);
+        }
+      });
+      
+    }, 150);    
+  });
+
+
+ //--------------each chart
+  if(winWidth <= 950) {
+  lWidth = 5;
+  tWidth = 4;
+  } else {
+    lWidth = 10;
+    tWidth = 8;
+  }
+
+  if(winWidth <= 1280) {
+    eachSize = 90;
+  } else {
+    eachSize = 100;
+ }
 
 
   const poData = [
@@ -57,11 +119,10 @@ document.addEventListener('DOMContentLoaded', function() {
   ];
 
 
-
   function startPie(){
     const mapData = poData.map(value => {
       // console.log(value.a);
-    
+      //document.querySelector(value.poKind +  ' .chart canvas').remove();
       var chart = window.chart = new EasyPieChart(document.querySelector(value.poKind + ' .chart'), {
       easing: 'easeOutElastic',
       delay: 3000,
@@ -70,6 +131,7 @@ document.addEventListener('DOMContentLoaded', function() {
       scaleColor: false,
       lineWidth: lWidth,
       trackWidth: tWidth,
+      size : eachSize,
       lineCap: 'round',
         onStep: function(from, to, percent) {
           this.el.children[0].innerHTML = Math.round(percent);
@@ -78,6 +140,10 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   }
   startPie();
+
+
+
+
 
 });
 
