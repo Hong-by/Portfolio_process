81fb2613393c221ec849a2206554b8cf9fff7381
HongBY <hby033@naver.com>
2021-06-14 18:34:05 +0900

    06/14 table-ui 부분 데이터를 json으로 변경하여 파싱하는 방법 example.hmtl에 정리 / 내용 정리 후 적용 필요

--- php/sp_insert.php
+++ php/sp_insert.php
@@ -26,6 +26,28 @@
 
   mysqli_query($dbConn, $sql);
 
+  //-----------------------------
+  $sql1 = "SELECT * FROM sp_table ORDER BY SP_idx DESC";
+
+  $sp_result = mysqli_query($dbConn, $sql1);
+
+  $arr = array();
+
+  while($sp_row = mysqli_fetch_array($sp_result)){
+    array_push($arr, array(
+      'sp_idx' => $sp_row['SP_idx'],
+      'sp_cate' => $sp_row['SP_cate'],
+      'sp_tit' => $sp_row['SP_tit'],
+      'sp_con' => $sp_row['SP_con'],
+      'sp_reg' => $sp_row['SP_reg']
+    ));
+  };
+
+  // var_dump($arr);
+  file_put_contents($_SERVER["DOCUMENT_ROOT"].'/schedule/data/sp_table.json', json_encode($arr, JSON_UNESCAPED_UNICODE));
+
+  //-------------------------------
+
   echo "
     <script>
       alert('작성이 완료되었습니다!');
